cf6c08579c7005d3a0e26d48e0edaac52bca9133
Anshuman Tiwari <anshumantiwari.cse@gmail.com>
2024-12-04 14:28:15 +0530

    up book page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,69 @@
+// import type { Metadata } from "next";
+// import { DM_Sans } from "next/font/google";
+// import "./globals.css";
+// import clsx from "clsx";
+
+// import { Footer } from "@/sections/Footer";
+// import WhatsApp from "@/components/globalIcons/WhatAppIcon";
+
+// const dmSans = DM_Sans({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: {
+//     default: "PK Photography-Best in photography and videography.",
+//     template: "%s - Best photography and videography service in mumbai",
+//   },
+//   description:
+//     "We offer Portrait, Event, Commercial, lifestyle, Corporate Headshots, Real Estate shoots and Wedding and Events Photography.",
+//   twitter: {
+//     card: "summary_large_image",
+//   },
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <>
+//       <html lang="en" className="relative">
+//         <head>
+//           <meta
+//             name="google-site-verification"
+//             content="xkkbCA30-55oudh-aQAwydfWkzUga0omPDIZ6lN9RB4"
+//           />
+//         </head>
+//         <body className={clsx(dmSans.className, "antialiased")}>
+//           {children}
+
+//           <Footer />
+//           <WhatsApp />
+//         </body>
+//       </html>
+//     </>
+//   );
+// }
+
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
-import Image from "next/image";
-import {Footer} from "@/sections/Footer";
+
+import { Footer } from "@/sections/Footer";
+import WhatsAppIcon from "@/components/globalIcons/WhatAppIcon";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
     default: "PK Photography-Best in photography and videography.",
-    template: "%s - Best photography and videography service in mumbai"
+    template: "%s - Best photography and videography service in mumbai",
   },
-  description: "We offer Portrait, Event, Commercial, lifestyle, Corporate Headshots, Real Estate shoots and Wedding and Events Photography.",
-  twitter:{
-    card: "summary_large_image"
+  description:
+    "We offer Portrait, Event, Commercial, lifestyle, Corporate Headshots, Real Estate shoots and Wedding and Events Photography.",
+  twitter: {
+    card: "summary_large_image",
   },
 };
 
@@ -33,7 +82,12 @@ export default function RootLayout({
       </head>
       <body className={clsx(dmSans.className, "antialiased")}>
         {children}
+
+        {/* Footer component */}
         <Footer />
+
+        {/* WhatsApp icon displayed globally */}
+        <WhatsAppIcon />
       </body>
     </html>
   );

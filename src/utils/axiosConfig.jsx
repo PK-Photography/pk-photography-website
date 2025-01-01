9cf9986b8ac529e0b4cbb77f8471f7f0c8264ae1
anshuman <anshuman.tiwari@kindasolutions.com>
2025-01-01 12:56:07 +0530

    update

--- src/utils/axiosConfig.jsx
+++ src/utils/axiosConfig.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 // const baseUR = process.env.DEV_API_URL;
 // const baseUR = import.meta.env.DEV_API_URL;
 
-const baseURL = "http://localhost:8085/api/v1";
+const baseURL = "https://pk-backend-jzxv.onrender.com/api/v1";
 // import.meta.env.VITE_XX
 // const baseURL = import.meta.env.VITE_APP_API_LIVE;
 

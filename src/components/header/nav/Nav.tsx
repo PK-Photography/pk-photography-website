056aff4b0076df1f45d1d22b60276e1a1537a469
Anshuman Tiwari <anshumantiwari.cse@gmail.com>
2025-01-02 21:43:34 +0530

    updated

--- src/components/header/nav/Nav.tsx
+++ src/components/header/nav/Nav.tsx
@@ -1,3 +1,76 @@
+// "use client";
+// import React, { useState } from "react";
+// import styles from "./style.module.scss";
+// import { motion } from "framer-motion";
+// import { usePathname } from "next/navigation";
+// import { menuSlide } from "../anim";
+// import Link1 from "./Link/Link1";
+// import Curve from "./Curve/Curve";
+// import Footer from "./Footer/Footer";
+
+// const navItems = [
+//   {
+//     title: "Home",
+//     href: "/",
+//   },
+//   {
+//     title: "Client",
+//     href: "/client",
+//   },
+//   {
+//     title: "Book Now",
+//     href: "/booking",
+//   },
+//   {
+//     title: "Blogs",
+//     href: "https://pkblogs-dev.onrender.com/",
+//   },
+//   {
+//     title: "Admin",
+//     href: "/Admin",
+//   },
+// ];
+
+// export default function Nav() {
+//   const pathname = usePathname();
+//   const [selectedIndicator, setSelectedIndicator] = useState(pathname);
+
+//   return (
+//     <motion.div
+//       variants={menuSlide}
+//       initial="initial"
+//       animate="enter"
+//       exit="exit"
+//       className={styles.menu}
+//     >
+//       <div className={styles.body}>
+//         <div
+//           onMouseLeave={() => {
+//             setSelectedIndicator(pathname);
+//           }}
+//           className={styles.nav}
+//         >
+//           <div className={styles.header}>
+//             <p>PKPhotography</p>
+//           </div>
+//           {navItems.map((data, index) => {
+//             return (
+//               <Link1
+//                 key={index}
+//                 data={{ ...data, index }}
+//                 isActive={selectedIndicator == data.href}
+//                 setSelectedIndicator={setSelectedIndicator}
+//               ></Link1>
+//             );
+//           })}
+//         </div>
+//         <Footer />
+//       </div>
+//       <Curve />
+//     </motion.div>
+//   );
+// }
+
 "use client";
 import React, { useState } from "react";
 import styles from "./style.module.scss";
@@ -22,8 +95,8 @@ const navItems = [
     href: "/booking",
   },
   {
-    title: "Blog",
-    href: "https://blogs.pkphotography.io/",
+    title: "Blogs",
+    href: "https://pkblogs-dev.onrender.com/",
   },
   {
     title: "Admin",
@@ -54,13 +127,15 @@ export default function Nav() {
             <p>PKPhotography</p>
           </div>
           {navItems.map((data, index) => {
+            const isExternal = data.href.startsWith("http");
             return (
               <Link1
                 key={index}
                 data={{ ...data, index }}
                 isActive={selectedIndicator == data.href}
                 setSelectedIndicator={setSelectedIndicator}
-              ></Link1>
+                target={isExternal ? "_blank" : "_self"} // Add target attribute
+              />
             );
           })}
         </div>

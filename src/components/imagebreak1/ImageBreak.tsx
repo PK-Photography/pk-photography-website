7988611cc1c982fe55f2bbf9c8aba399db803a71
Anshuman Tiwari <anshumantiwari.cse@gmail.com>
2025-01-22 22:40:46 +0530

    updated headings

--- src/components/imagebreak1/ImageBreak.tsx
+++ src/components/imagebreak1/ImageBreak.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import BubbleText from "@/components/BubbleText/BubbleText";
 // import Carousel from "./Carousel";
 import Slideshow from "./SlideShow";
+import { BubbleText } from "../BubbleText/BubbolTextProps";
 
 export const ImageBreak = () => {
   return (
     <div className="container mx-auto p-4 md:p-10 bg-[#eae8e4] flex flex-col items-center">
-      <BubbleText />
+      <BubbleText text="Visual Stories" id="Visual-Stories" />
       <div className="w-full max-w-5xl mt-5 overflow-hidden">
         {/* <Carousel /> */}
         <Slideshow />

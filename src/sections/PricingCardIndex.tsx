7474fb8d35869a6e9230ccc8b5d041ab980adb02
Anshuman Tiwari <anshumantiwari.cse@gmail.com>
2025-01-27 22:36:32 +0530

    updated photos and analyic

--- src/sections/PricingCardIndex.tsx
+++ src/sections/PricingCardIndex.tsx
@@ -8,47 +8,52 @@ const packages = [
     title: "PREMIUM PACKAGE",
     price: "PRICE: 20,000 INR",
     images: [
-      "/carouselLandscape/img1.jpg",
-      "/carouselLandscape/img2.jpg",
-      "/carouselLandscape/img3.jpg",
-      "/carouselLandscape/img4.jpg",
-      
+      "/pricing/cover.jpg",
+      "/pricing/PKP_2826.jpg",
+      "/pricing/PKP_7172.jpg",
     ],
     shootType: "FULL PORTFOLIO SHOOT",
     duration: "Duration: 5 To 6 Hours",
     looks: "Looks: 5 Different Looks",
-    inclusions: "Inclusions: Studio, Shooting, Editing, And Hair & Makeup Artist Costs",
-    deliverables: "Deliverables: 20 Edited Photographs, All Raw Photos Via Drive Link",
+    inclusions:
+      "Inclusions: Studio, Shooting, Editing, And Hair & Makeup Artist Costs",
+    deliverables:
+      "Deliverables: 20 Edited Photographs, All Raw Photos Via Drive Link",
     note: "(Note: Dresses Not Included)",
   },
   {
     title: "STANDARD PACKAGE",
     price: "PRICE: 15,000 INR",
     images: [
-        "/carouselLandscape/img6.jpg",
-        "/carouselLandscape/img7.jpg",
-        "/carouselLandscape/img5.jpg",
+      "/pricing/0N3A7946.jpg",
+      "/pricing/PKP_7916l.jpg",
+      "/pricing/PKP_8780pl.jpg",
     ],
     shootType: "MINI PORTFOLIO SHOOT",
     duration: "Duration: 3 To 4 Hours",
     looks: "Looks: 3 Different Looks",
-    inclusions: "Inclusions: Studio, Shooting, Editing, And Hair & Makeup Artist Costs",
-    deliverables: "Deliverables: 15 Edited Photographs, All Raw Photos Via Drive Link",
+    inclusions:
+      "Inclusions: Studio, Shooting, Editing, And Hair & Makeup Artist Costs",
+    deliverables:
+      "Deliverables: 15 Edited Photographs, All Raw Photos Via Drive Link",
     note: "(Note: Dresses Not Included)",
   },
   {
     title: "STARTER PACKAGE",
     price: "PRICE: 5,000 INR",
     images: [
-        "/carouselLandscape/img5.jpg",
-        "/carouselLandscape/img4.jpg",
-        "/carouselLandscape/img3.jpg",
+      "/pricing/5S1A9900 cover.jpg",
+      "/pricing/Anushka 4.jpg",
+      "/pricing/PKP_0763 cover.jpg",
+      "/pricing/PKP_551.jpg",
     ],
     shootType: "ONE-HOUR PORTRAIT SESSION",
     duration: "Duration: 1 Hour",
     details: "Details: Unlimited Dress Changes Within The Time Frame",
-    inclusions: "Inclusions: Shooting And Editing Costs (Hair & Makeup Artist Not Included)",
-    deliverables: "Deliverables: 5 Edited Photographs, All Raw Photos Via Drive Link",
+    inclusions:
+      "Inclusions: Shooting And Editing Costs (Hair & Makeup Artist Not Included)",
+    deliverables:
+      "Deliverables: 5 Edited Photographs, All Raw Photos Via Drive Link",
     note: "(Note: Dresses Not Included)",
   },
 ];

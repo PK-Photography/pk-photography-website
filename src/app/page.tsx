9ef44b0bcbe0fda31179da5c9a04bddc04742b8d
Toshita Pandey <toshitapandey@gmail.com>
2025-04-11 04:17:46 +0530

    remove parallax + add about our studio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,106 +1,15 @@
-// "use client";
-// import { Header1 } from "@/sections/Header1";
-// import { Hero } from "@/sections/Hero";
-// import { LogoTicker } from "@/sections/LogoTicker";
-// // import { ProductShowcase } from "@/sections/ProductShowcase";
-
-// // import { Testimonials } from "@/sections/Testimonials";
-// import { CallToAction } from "@/sections/CallToAction";
-// import Preloader from "@/sections/Preloader";
-// import { useEffect, useState } from "react";
-// import Header from "@/components/header/Header";
-// import Service from "@/components/services/Service";
-// // import { DragCards } from "@/components/Drag/Drag";
-// import { TextParallaxContentExample } from "@/components/WeAlsoDo/TextParallaxContent";
-// import Featured from "@/components/Featured/Featured";
-// import { ImageBreak } from "@/components/imagebreak1/ImageBreak";
-// import { Do } from "@/components/WhatWeDo/Do";
-// import Reviews from "@/sections/Reviews";
-// import { BubbleText } from "@/components/BubbleText/BubbolTextProps";
-// import GoogleReviews from "@/sections/GoogleReviews";
-// import FeaturedGallery from "@/components/featuredCard/FeaturedGallery";
-// import Image from "next/image";
-// import Logo from "@/assets/logo.webp";
-// import OurClients from "@/sections/OurClients";
-// import PricingCardIndex from "@/sections/PricingCardIndex";
-
-// // import Project  from "@/sections/Project";
-
-// export default function Home() {
-//   const [showPreloader, setShowPreloader] = useState(true);
-
-//   useEffect(() => {
-//     setTimeout(() => {
-//       setShowPreloader(false);
-//     }, 6000);
-//   }, []);
-
-//   return (
-//     <>
-//       {showPreloader ? (
-//         <Preloader />
-//       ) : (
-//         <div className="bg-[#eae8e4]">
-//           {/* <Header1/> */}
-//           <Header />
-//           <div className="top-0 left-10 md:top-0 md:left-8">
-//             <Image
-//               src={Logo}
-//               alt="Saas Logo"
-//               width={120} // Set width to control aspect ratio
-//               height={120} // Set height to control aspect ratio
-//               className="w-[80px] h-[80px] md:w-[120px] md:h-[120px] object-contain  z-50"
-//             />
-//           </div>
-
-//           <Hero />
-//           <ImageBreak />
-//           {/* <GoogleReviews /> */}
-//           <BubbleText text="Featured Projects" id="Featured-Projects" />
-
-//           <FeaturedGallery />
-//           {/* <LogoTicker/> */}
-//           <Service />
-//           {/* <ProductShowcase/> */}
-//           <TextParallaxContentExample />
-//           {/* <Project/> */}
-//           <Do />
-//           {/* <Featured/> */}
-//           {/* <Testimonials /> */}
-//           {/* <CallToAction /> */}
-//           <BubbleText text="Testimonial" id="#Testimonial" />
-//           <Reviews />
-//           <BubbleText text="Our Clients" id="#Our-Clients" />
-//           <OurClients />
-//           <BubbleText text="Shoot Pricing " id="#Shoot-Pricing" />
-//           <PricingCardIndex />
-//         </div>
-//       )}
-//     </>
-//   );
-// }
 "use client";
 import { useEffect } from "react";
 
 import { Hero } from "@/sections/Hero";
-import { LogoTicker } from "@/sections/LogoTicker";
-// import { ProductShowcase } from "@/sections/ProductShowcase";
-
-// import { Testimonials } from "@/sections/Testimonials";
-import { CallToAction } from "@/sections/CallToAction";
 import Header from "@/components/header/Header";
 import Service from "@/components/services/Service";
-// import { DragCards } from "@/components/Drag/Drag";
 import { TextParallaxContentExample } from "@/components/WeAlsoDo/TextParallaxContent";
-import Featured from "@/components/Featured/Featured";
 import { ImageBreak } from "@/components/imagebreak1/ImageBreak";
 import { Do } from "@/components/WhatWeDo/Do";
 import Reviews from "@/sections/Reviews";
+import AboutStudio from "@/components/Home/AboutStudio";
 import { BubbleText } from "@/components/BubbleText/BubbolTextProps";
-import GoogleReviews from "@/sections/GoogleReviews";
-import FeaturedGallery from "@/components/featuredCard/FeaturedGallery";
-import Image from "next/image";
-import Logo from "@/assets/logo.webp";
 import OurClients from "@/sections/OurClients";
 import PricingCardIndex from "@/sections/PricingCardIndex";
 
@@ -118,36 +27,17 @@ export default function Home() {
 
   return (
     <div className="bg-[#eae8e4]">
-      {/* <Header1/> */}
       <Header />
-      {/* <div className="top-0 left-10 md:top-0 md:left-8">
-        <Image
-          src={Logo}
-          alt="Saas Logo"
-          width={120} // Set width to control aspect ratio
-          height={120} // Set height to control aspect ratio
-          className="w-[80px] h-[80px] md:w-[120px] md:h-[120px] object-contain  z-50"
-        />
-      </div> */}
-
       <Hero />
       <ImageBreak />
-      {/* <BubbleText text="Featured Projects" id="Featured-Projects" /> */}
-      {/* <FeaturedGallery /> */}
-      {/* <LogoTicker/> */}
       <BubbleText text="Our Services" id="Our-Services" />
       <Service />
-      {/* <ProductShowcase/> */}
-      <TextParallaxContentExample />
-      {/* <Project/> */}
       <Do />
-      {/* <Featured/> */}
-      {/* <Testimonials /> */}
-      {/* <CallToAction /> */}
-      <BubbleText text="Testimonial" id="Testimonial" />
-      <Reviews />
       <BubbleText text="Our Clients" id="Our-Clients" />
       <OurClients />
+      <BubbleText text="Testimonial" id="Testimonial" />
+      <Reviews />
+      <AboutStudio/>
       <BubbleText text="Shoot Pricing" id="Shoot-Pricing" />
       <PricingCardIndex />
     </div>

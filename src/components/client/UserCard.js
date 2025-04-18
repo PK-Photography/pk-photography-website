8fd4f999f4ddf5f1a09e17938bc0010110f53839
Toshita Pandey <toshitapandey@gmail.com>
2025-04-18 15:45:20 +0530

    add single card in mobile

--- src/components/client/UserCard.js
+++ src/components/client/UserCard.js
@@ -123,7 +123,7 @@ const UserCards = () => {
           </div>
         ) : (
           <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 md:gap-10">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-6 sm:gap-8 md:gap-10">
               {paginatedCards.map((card) => (
                 <button
                   key={card._id}

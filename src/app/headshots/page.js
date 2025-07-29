a759cd596822cc1f17c2e2eb1953702c7d22849b
Puneet Jain <jainpuneet711@gmail.com>
2025-07-29 19:05:21 +0530

    feat:partial page ready

---
+++ src/app/headshots/page.js
@@ -0,0 +1,98 @@
+"use client";
+import Image from "next/image";
+import img1 from "@/assets/portfolio/img1.png";
+import img2 from "@/assets/portfolio/img2.png";
+import img3 from "@/assets/portfolio/img3.png";
+import img4 from "@/assets/portfolio/img4.png";
+import img5 from "@/assets/portfolio/img5.png";
+import img6 from "@/assets/portfolio/img6.png";
+import img7 from "@/assets/portfolio/img7.png";
+
+import LocationStylingTipsSec from "@/sections/serviceSections/LocationStylingTipsSec";
+import ExpertPhotography from "@/sections/serviceSections/ExpertPhotography";
+import HighEndResolution from "@/sections/serviceSections/HighEndResolution";
+import MoodBoardSectionV2 from "@/sections/serviceSections/MoodBoardSectionV2";
+import WardrobeAndProp from "@/sections/serviceSections/WardrobeAndProp";
+import AboutSection from "@/sections/serviceSections/AboutSection";
+import GetStartedSection from "@/sections/serviceSections/GetStartedSection";
+
+export default function Headshots() {
+  return (
+    <div className="pl-6 pr-6 ">
+      <div className="flex flex-row justify-between pb-9 border-b-2 border-[#747478] ">
+        <div className="text-6xl font-semibold">
+          <p className="pb-2.5 pt-3">Headshot</p>
+          <p>Photography</p>
+        </div>
+        <div className="flex flex-col text-right gap-4">
+          <div className="flex justify-between items-center text-[#747478] font-bold border-b-2 border-[#747478] pb-4">
+            <span className="pr-4">GALLERY</span>
+            <span className="ml-20">&gt;</span>
+          </div>
+          <div className="flex justify-between items-center text-[#747478] font-bold border-b-2 border-[#747478] pb-4">
+            <span>PORTFOLIO</span>
+            <span className="ml-2">&gt;</span>
+          </div>
+          <div className="flex justify-between items-center text-[#747478] font-bold  pb-4">
+            <span>PROFILE</span>
+            <span className="ml-2">&gt;</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-6 flex flex-col md:flex-row mb-[10%]">
+        <div className="flex flex-col justify-start items-start border-r-2 border-[#747478] pr-12">
+          <p className="pt-8">
+            Let us help you create the perfect professional image.
+          </p>
+          <p className="pt-1">
+            Personalized headshot photography to showcase your{" "}
+          </p>
+          <p className="pt-1">
+            personality. Your journey to a confident headshot starts here.
+          </p>
+
+          <button className="mt-8 py-3 px-8 bg-black text-white rounded-full">
+            BOOK NOW
+          </button>
+        </div>
+        <Image
+          src={img1}
+          alt="Headshot 1"
+          className="w-[40%] h-[40%] rounded-lg pl-5 mt-4"
+        />
+        <Image
+          src={img2}
+          alt="Headshot 2"
+          className="w-[30%] h-[40%] rounded-lg  pl-5 mt-5"
+        />
+      </div>
+      <GetStartedSection
+        text1="Elevate Your Presence"
+        text2=" with Professional"
+        text3="Headshots"
+      />
+      <AboutSection imageUrl={img3} />
+      <WardrobeAndProp />
+      <div className="mt-[10%] flex flex-row justify-center items-center">
+        <Image
+          src={img4}
+          alt="Headshot 4"
+          className="w-[65%] h-[50%] sl:w-[20%] sl:h-[20%] object-cover mt-10 pl-5 "
+        />
+      </div>
+      <MoodBoardSectionV2 imageUrl={img5} />
+      <LocationStylingTipsSec />
+      <ExpertPhotography imageUrl={img6} />
+      <HighEndResolution imageUrl={img7} />
+      <div className="flex flex-col items-center justify-center mt-10">
+        <p className="text-[25px]">Final Delivery</p>
+        <p className="text-[15px] text-[#5C5C5C] mt-4 max-w-3xl text-center">
+          The moment of final delivery marks the culmination of our dedicated
+          efforts and your envisioned <br /> outcome brought to life. It&apos;s
+          more than just a transaction.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/sections/serviceSections/GetStartedSection.tsx
+++ src/sections/serviceSections/GetStartedSection.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 
-const GetStartedSection: React.FC = () => {
+type GetStartedSectionProps = {
+  text1: string;
+  text2: string;
+  text3: string;
+};
+
+const GetStartedSection: React.FC<GetStartedSectionProps> = ({
+  text1,
+  text2,
+  text3,
+}) => {
   return (
     <div className="bg-[#f7f7f7] py-12 px-6 border-l-[6px] border-yellow-500">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-start space-y-8 md:space-y-0 md:space-x-12">
         {/* Left Section - Heading and Button (Stacks on Mobile) */}
         <div className="w-full md:w-1/3  md:text-left">
           <h2 className="text-2xl md:text-3xl font-light text-[#000000] leading-snug">
-            Showcase Your <br className="hidden md:block" />
-            Talent with a <br className="hidden md:block" />
-            Stunning Portfolio
+            {text1} <br className="hidden md:block" />
+            {text2} <br className="hidden md:block" />
+            {text3}
           </h2>
 
           <button className="lg:flex hidden mt-4 md:mt-6 px-6 py-2 text-sm font-semibold border border-gray-800 rounded-md transition duration-300 shadow-[-6px_6px_0px_#000] hover:shadow-[-4px_4px_0px_#000] active:shadow-[-2px_2px_0px_#000] active:scale-95">
